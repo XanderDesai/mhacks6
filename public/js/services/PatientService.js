9663b0e4588b5210f18b6b6074dd241b5b2b755e
Liuxizi Xu <xlxzbisulyn@gmail.com>
2015-09-12 18:19:03 -0400

    front end connected to mongodb

--- public/js/services/PatientService.js
+++ public/js/services/PatientService.js
@@ -1,30 +1,3 @@
-// angular.module('PatientService', []).factory('Patients', ['$http', function($http) {
-		return {
-			// // get all patients
-   //          get : function() {
-   //              return $http.get('/api/patients');
-   //          },
-   //          // create a patient
-   //          create : function(patientData) {
-   //              return $http.post('/api/patients', patientData);
-   //          },
-   //          // get one patient
-   //          get : function(id) {
-   //              return $http.get('/api/patients/' + id);
-   //          },
-   //          // update one patient
-   //          put : function(id) {
-   //          	return $http.put('/api/patients/' + id);
-   //          },
-   //          // delete a patient
-   //          delete : function(id) {
-   //              return $http.delete('/api/patients/' + id);
-   //          }
-
-        }
-// 	}
-// });
-
 angular.module('PatientService', []).factory('Patients', ['$http', function($http) {
 			return {
 			// get all patients
@@ -35,17 +8,18 @@ angular.module('PatientService', []).factory('Patients', ['$http', function($htt
             create : function(patientData) {
                 return $http.post('/api/patients', patientData);
             },
-            // // get one patient
-            // get : function(id) {
-            //     return $http.get('/api/patients/' + id);
-            // },
-            // // update one patient
-            // put : function(id) {
-            // 	return $http.put('/api/patients/' + id);
-            // },
+            // get one patient
+            get : function(id) {
+                return $http.get('/api/patients/' + id);
+            },
+            // update one patient
+            put : function(id) {
+            	return $http.put('/api/patients/' + id);
+            },
             // delete a patient
             delete : function(id) {
                 return $http.delete('/api/patients/' + id);
             }
+
         }
 }]);
