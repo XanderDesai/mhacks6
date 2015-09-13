12bd64905b1ab3c8c64317db7976ae4a3ea0e3e0
Alexander <XanderDesai@me.com>
2015-09-12 21:48:24 -0400

    Fixed my bugs with Patient Services

--- public/js/controllers/MapController.js
+++ public/js/controllers/MapController.js
@@ -1,8 +1,8 @@
 app.controller('MapController', ['$scope', '$http', 'PatientService', function($scope,$http, PatientService)
 	{
 
-		$scope.sendSMS {
+		$scope.sendSMS = function() {
 			var results = PatientService.get();
 			console.log(results);
 		}
-	});
+	}]);
